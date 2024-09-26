1ba42af1647da4f4549c924abf032df0e0d36329
Andrew St. Jean <astjean@symplr.com>
2024-09-26 15:53:03 -0500

    refactor: create sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,53 @@
 import "./App.scss";
 
-import { Card } from "primereact/card";
 import Header from "./header/Header";
 
 function App() {
   return (
     <div className="app">
-      <Header></Header>
       <section className="app-content">
-        <div className="grid">
-          <div className="col-12 md:col-6 lg:col-3">
-            <Card>A</Card>
-          </div>
-          <div className="col-12 md:col-6 lg:col-3">
-            <Card>A</Card>
-          </div>
-          <div className="col-12 md:col-6 lg:col-3">
-            <Card>A</Card>
-          </div>
-          <div className="col-12 md:col-6 lg:col-3">
-            <Card>A</Card>
+        <div className="app__side__content flex-column bg-blue-900 z-5 fixed top-0 bottom-0">
+          <div className="flex flex-column flex-grow-1 p-2 gap-3 overflow-y-auto">
+            <div className="flex flex-shrink-0">Logo</div>
+            <nav className="flex flex-column flex-1">
+              <ul className="flex flex-column flex-1">
+                <li>
+                  <ul>
+                    <li>Test</li>
+                    <li>Test</li>
+                    <li>Test</li>
+                  </ul>
+                </li>
+                <li className="mt-auto">Bottom</li>
+              </ul>
+            </nav>
           </div>
         </div>
+        <div className="app__content">
+          <Header></Header>
+          <main></main>
+          <footer className="app-footer p-2 bg-blue-900">
+            Under construction <i className="pi pi-spin pi-spinner"></i>
+          </footer>
+        </div>
+        {/* <div className="grid">
+          <div className="col-12 md:col-6 lg:col-3">
+            <Card>A</Card>
+          </div>
+          <div className="col-12 md:col-6 lg:col-3">
+            <Card>A</Card>
+          </div>
+          <div className="col-12 md:col-6 lg:col-3">
+            <Card>A</Card>
+          </div>
+          <div className="col-12 md:col-6 lg:col-3">
+            <Card>A</Card>
+          </div>
+        </div> */}
       </section>
-      <footer className="app-footer p-2 bg-blue-900">
+      {/* <footer className="app-footer p-2 bg-blue-900">
         Under construction <i className="pi pi-spin pi-spinner"></i>
-      </footer>
+      </footer> */}
     </div>
   );
 }
--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -4,9 +4,16 @@ import { Avatar } from "primereact/avatar";
 
 function Header() {
   return (
-    <header className="flex justify-content-between flex-wrap bg-blue-900">
-      <h3 className="flex align-items-center justify-content-center m-2">Always under construction</h3>
-      <Avatar className="flex align-items-center justify-content-center m-2" size="large" shape="circle" image="https://avatars.githubusercontent.com/u/17767531"/>
+    <header className="flex justify-content-between flex-wrap bg-blue-500">
+      <h3 className="flex align-items-center justify-content-center m-2">
+        Always under construction
+      </h3>
+      <Avatar
+        className="flex align-items-center justify-content-center m-2"
+        size="large"
+        shape="circle"
+        image="https://avatars.githubusercontent.com/u/17767531"
+      />
     </header>
   );
 }
