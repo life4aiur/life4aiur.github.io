883bcf7799102970db553c1268d22caa22cc7f48
Andrew St. Jean <astjean@symplr.com>
2024-09-27 14:51:41 -0500

    refactor: pull sidebar into it's own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,13 @@
 import "./App.scss";
 
 import Header from "./header/Header";
+import Sidebar from "./sidebar/Sidebar";
 
 function App() {
   return (
     <div className="app">
       <section className="app-content">
-        <div className="app__side__content flex-column bg-blue-900 z-5 fixed top-0 bottom-0">
-          <div className="flex flex-column flex-grow-1 p-2 gap-3 overflow-y-auto">
-            <div className="flex flex-shrink-0">Logo</div>
-            <nav className="flex flex-column flex-1">
-              <ul className="flex flex-column flex-1">
-                <li>
-                  <ul>
-                    <li>Test</li>
-                    <li>Test</li>
-                    <li>Test</li>
-                  </ul>
-                </li>
-                <li className="mt-auto">Bottom</li>
-              </ul>
-            </nav>
-          </div>
-        </div>
+        <Sidebar></Sidebar>
         <div className="app__content">
           <Header></Header>
           <main></main>
---
+++ src/sidebar/Sidebar.tsx
@@ -0,0 +1,36 @@
+import { Avatar } from "primereact/avatar";
+import "./Sidebar.scss";
+
+function Sidebar() {
+  return (
+    <div className="sidebar__content flex-column bg-blue-900 z-5 fixed top-0 bottom-0">
+      <div className="flex flex-column flex-grow-1 p-2 gap-3 overflow-y-auto">
+        <Avatar
+          className="flex flex-shrink-0 align-self-center m-2 w-7"
+          size="xlarge"
+          shape="circle"
+          image="https://avatars.githubusercontent.com/u/17767531"
+        />
+        <p className="flex flex-column flex-1 px-2">
+          <h3 className="align-self-center">Hello there!</h3>
+          My name is Andrew. I'm a software developer with a passion for
+          front-end development, Star Wars, and generally silly things.
+        </p>
+        <nav className="flex flex-column flex-1">
+          <ul className="flex flex-column flex-1">
+            <li>
+              <ul>
+                <li>About</li>
+                <li>Work</li>
+                <li>Contact</li>
+              </ul>
+            </li>
+            <li className="mt-auto">Github</li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
