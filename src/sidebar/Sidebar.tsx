025beb9cbc7053416b12f2cfa49adfa5a6e62d42
life4aiur <astjean12@gmail.com>
2024-09-30 13:08:46 -0500

    refactor: misc code improvements

--- src/sidebar/Sidebar.tsx
+++ src/sidebar/Sidebar.tsx
@@ -6,12 +6,12 @@ function Sidebar() {
     <div className="sidebar__content flex-column bg-blue-900 z-5 fixed top-0 bottom-0">
       <div className="flex flex-column flex-grow-1 p-2 gap-3 overflow-y-auto">
         <Avatar
-          className="flex flex-shrink-0 align-self-center m-2 w-7"
+          className="flex flex-shrink-0 align-self-center m-2 w-7 h-auto"
           size="xlarge"
           shape="circle"
           image="https://avatars.githubusercontent.com/u/17767531"
         />
-        <p className="flex flex-column flex-1 px-2">
+        <p className="flex flex-column px-2">
           <h3 className="align-self-center">Hello there!</h3>
           My name is Andrew. I'm a software developer with a passion for
           front-end development, Star Wars, and generally silly things.
